43966e7ef6ba4a77104e7a445dd48186509bd1da
Ngannguyendt2 <ngannguyendt2haui@gmail.com>
2019-08-22 08:19:02 +0700

    validate email

---
+++ main.js
@@ -0,0 +1,10 @@
+function checkvalidateEmail(str) {
+    let regexp=/^[A-Za-z0-9]+[A-Za-z0-9]*@[A-Za-z0-9]+(\.[A-Za-z0-9]+)$/;
+    if(regexp.test(str))
+    {
+        return true;
+    }
+    return false;
+
+}
+console.log(checkvalidateEmail("@#abc@gmail.com"));
